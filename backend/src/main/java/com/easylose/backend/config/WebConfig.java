58bcc464401b1f74caffd434737b1635d4ab2aae
tokataka <skygazer227@gmail.com>
2022-09-20 18:48:30 +0900

    FEAT: Add Social login and token stuffs

---
+++ backend/src/main/java/com/easylose/backend/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.easylose.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+    .allowedMethods("GET", "POST", "PUT", "DELETE")
+    .exposedHeaders("Access-Token")
+    .maxAge(3000);
+  }
+}
